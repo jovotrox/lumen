ab36fa6de4cde17ae71dd3f016659bfea5dd7f7d
jovotrox <jovotrox@users.noreply.github.com>
2026-01-26 03:25:57 +0000

    deploy: fcf8e8577a73b944a57dd90df4da679f614b408e

--- assets/index-B3J0780W.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-DSbPIzP2.js";async function p(i,n){await o("plugin:shell|open",{path:i,with:n})}export{p as open};
---
+++ assets/index-DzC6n5HG.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index-t0zbbOwq.js";async function p(i,n){await o("plugin:shell|open",{path:i,with:n})}export{p as open};
