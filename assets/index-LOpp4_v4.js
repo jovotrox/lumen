f9f5c44d18ed6ed9e832f3c94bee004f5f7d5a3d
jovotrox <jovotrox@users.noreply.github.com>
2026-01-27 02:46:52 +0000

    deploy: 8daaa6e3e6a4cb5bd4c05e2fea0e50bb822c6c93

---
+++ assets/index-LOpp4_v4.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index-DA_K2Q-J.js";async function p(i,n){await o("plugin:shell|open",{path:i,with:n})}export{p as open};
--- assets/index-v_Mqv-pv.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-UxU75ivc.js";async function p(i,n){await o("plugin:shell|open",{path:i,with:n})}export{p as open};
